aee85819b70b6d9f4e259dbb1e89751df371cc6e
Tom Brooks <tom@olospo.co.uk>
2026-04-28 15:34:25 +0100

    Basic templates added

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style(
+        'mhid-parent',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_style(
+        'mhid-aim',
+        get_stylesheet_directory_uri() . '/style.css',
+        ['mhid-parent'],
+        wp_get_theme()->get('Version')
+    );
+
+    wp_enqueue_script(
+        'aim-library',
+        get_stylesheet_directory_uri() . '/js/aim-library.js',
+        [],
+        wp_get_theme()->get('Version'),
+        true
+    );
+});
