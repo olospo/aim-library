22e320272f2b44fb2f553b78e3465c094efce7fe
Tom Brooks <tom@olospo.co.uk>
2026-05-08 12:44:05 +0100

    Prototype added

--- functions.php
+++ functions.php
@@ -7,4 +7,18 @@ add_action('wp_enqueue_scripts', function () {
         [],
         filemtime(get_stylesheet_directory() . '/style.css')
     );
+    
+    if (is_page_template([
+        'templates/aim-library.php',
+        'templates/aim-measure.php',
+    ])) {
+        wp_enqueue_style(
+            'aim-prototype',
+            get_stylesheet_directory_uri() . '/css/library.css',
+            [],
+            filemtime(get_stylesheet_directory() . '/css/library.css')
+        );
+    }
+    
+    
 }, 20);
