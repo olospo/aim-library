1d127c5a38bb7bcab8a088b2b5206f6abcf98994
Tom Brooks <tom@olospo.co.uk>
2026-05-08 10:11:24 +0100

    Fixes to child theme

--- functions.php
+++ functions.php
@@ -1,23 +1,12 @@
 <?php
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style(
-        'mhid-parent',
-        get_template_directory_uri() . '/css/main.css'
-    );
 
     wp_enqueue_style(
-        'mhid-aim',
-        get_stylesheet_directory_uri() . '/style.css',
-        ['mhid-parent'],
-        wp_get_theme()->get('Version')
-    );
-
-    wp_enqueue_script(
         'aim-library',
-        get_stylesheet_directory_uri() . '/js/aim-library.js',
+        get_stylesheet_uri(),
         [],
-        wp_get_theme()->get('Version'),
-        true
+        filemtime(get_stylesheet_directory() . '/style.css')
     );
-});
+
+}, 20);
