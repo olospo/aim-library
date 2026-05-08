7138755f9328311cbd35bda6e050b8fbc70bd0b7
Tom Brooks <tom@olospo.co.uk>
2026-05-08 10:14:43 +0100

    v0.1.1

--- functions.php
+++ functions.php
@@ -1,12 +1,10 @@
 <?php
 
 add_action('wp_enqueue_scripts', function () {
-
     wp_enqueue_style(
         'aim-library',
         get_stylesheet_uri(),
         [],
         filemtime(get_stylesheet_directory() . '/style.css')
     );
-
 }, 20);
