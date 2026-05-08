f95a2b68553d037b7b870d7f85c3bde8a9c066f9
Tom Brooks <tom@olospo.co.uk>
2026-05-08 09:54:38 +0100

    Update main css location

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style(
         'mhid-parent',
-        get_template_directory_uri() . '/style.css'
+        get_template_directory_uri() . '/css/main.css'
     );
 
     wp_enqueue_style(
