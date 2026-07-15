52fb74bfad593373d28afcaa610f010eb895153a
Tom Brooks <tom@olospo.co.uk>
2026-07-15 10:45:27 +0100

    Add navigation menu

--- template-parts/library-nav.php
+++ template-parts/library-nav.php
@@ -26,9 +26,27 @@ $my_url   = home_url( '/my-measures/' );
           <li>
             <a class="<?php echo is_page( 'submit-measure' ) ? 'active' : ''; ?>" href="<?php echo esc_url( $submit_url ); ?>">Submit Measure</a>
           </li>
-            <li>
-                <a class="<?php echo is_page( 'submit-measure' ) ? 'active' : ''; ?>" href="<?php echo esc_url( $my_url ); ?>">My Measures</a>
-            </li>
+          <li>
+            <a class="<?php echo is_page( 'submit-measure' ) ? 'active' : ''; ?>" href="<?php echo esc_url( $my_url ); ?>">My Measures</a>
+          </li>
+            
+          <?php // Comment on measure (current measure is pre-populated)
+          $comment_url = home_url( '/library-of-measures/comment-on-measure/' );
+          
+          if ( is_singular( 'measure' ) ) {
+            $comment_url = add_query_arg(
+              [
+                'aim_measure_title' => get_the_title(),
+                'aim_measure_id'    => get_the_ID(),
+              ],
+              $comment_url
+            );
+          }
+          ?>
+      
+          <li>
+            <a class="<?php echo is_page( 'comment-on-measure' ) ? 'active' : ''; ?>" href="<?php echo esc_url( $comment_url ); ?>">Comment on Measure</a>
+          </li>
 
           <li>
             <a href="<?php echo esc_url( wp_logout_url( $library_url ) ); ?>">Log Out</a>
